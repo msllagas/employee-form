848750e46e8eacf95905ddadee3ac86e8ef2ca86
msllagas <mndyllagas@gmail.com>
2023-05-05 23:21:16 +0800

    Added more stylings and forms

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const fileUpload = document.getElementById('fileupload');
+
+const fileChosen = document.getElementById('filechosen');
+
+fileUpload.addEventListener('change', function(){
+  fileChosen.textContent = this.files[0].name;
+  fileChosen.style.padding = '8px'
+})
+
